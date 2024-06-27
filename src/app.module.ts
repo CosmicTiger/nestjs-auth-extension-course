f9d7e72c569a87ff18e76403f6c0161a96095a2f
CosmicTiger <luisangelmarcia@gmail.com>
2024-06-27 16:04:10 -0600

    :sparkles: feat: add setup for basic codebase for the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoffeesModule } from './coffees/coffees.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    CoffeesModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'illuvcoffee',
+      password: '1lluvC0ff33&C0d!n5',
+      database: 'illuvcoffee_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

b1c1d77735ab34d88c28d3993755d0f2cd72c5ab
CosmicTiger <luisangelmarcia@gmail.com>
2024-06-27 16:46:32 -0600

    :sparkles: feat: add first logic on authentication

---
+++ src/iam/authentication/authentication.service.ts
@@ -0,0 +1,57 @@
+import {
+  ConflictException,
+  HttpCode,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashingService } from '../hashing/hashing.service';
+import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
+
+@Injectable()
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto): Promise<User> {
+    try {
+      const user = new User();
+      user.email = signUpDto.email;
+      user.username = signUpDto.username || signUpDto.email.split('@')[0];
+      user.password = await this.hashingService.hash(signUpDto.password);
+      return await this.usersRepository.save(user);
+    } catch (error) {
+      const pgUniqueViolationCErrorCode = '23505';
+      if (error.code === pgUniqueViolationCErrorCode) {
+        throw new ConflictException();
+      }
+      throw error;
+    }
+  }
+
+  @HttpCode(HttpStatus.OK)
+  async signIn(signInDto: SignInDto) {
+    const user = await this.usersRepository.findOneBy({
+      email: signInDto.email,
+    });
+    if (!user) {
+      throw new UnauthorizedException('User does not exists');
+    }
+    const isPasswordValid = await this.hashingService.compare(
+      signInDto.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
+
+    return true;
+  }
+}
